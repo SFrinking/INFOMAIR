85fce1e48637635855b9066cab9abf7832331a61
Luca Lin <agahamsc@live.com>
2020-09-12 00:42:40 +0200

    plot confusion matrix using matplotlib

--- classification.py
+++ classification.py
@@ -35,12 +35,100 @@ X_test_vectorized=vectorizer.transform(X_test)
 
 #classifier logistic regression
 
+def plot_confusion_matrix(cm,target_names,title='Confusion matrix', cmap=None,normalize=False):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
 
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(12, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+
+def Convert(lst): 
+    counts = {}
+    for i in lst:
+      counts[i] = counts.get(i, 0) + 1
+    return counts
+
+
+def make_confusion_matrix(y_predicted):
+    dictionary=Convert(y_test)
+    print(dictionary)
+    #create confusion matrix and plot
+    from sklearn.metrics import confusion_matrix
+    labels_no_duplicates= list(dict.fromkeys(labels))
+    cm=confusion_matrix(y_test,y_predicted, labels_no_duplicates)
+    plot_confusion_matrix(cm,labels_no_duplicates)
+    
 #get evaluation metrics
 def EvalMetrics(y_predicted):
-    #create confusion matrix
-    from sklearn.metrics import confusion_matrix
-    cm=confusion_matrix(y_test,y_predicted)
+    
+    make_confusion_matrix(y_predicted)
     
     from sklearn.metrics import recall_score
     mi_recall=recall_score(y_test, y_predicted, average="micro")
