a2130f60c4cdbe8e69ab24dc2d07c319704cef7b
Luca Lin <agahamsc@live.com>
2020-09-12 13:26:07 +0200

    added eval metrics to plt plots

--- classification.py
+++ classification.py
@@ -28,14 +28,19 @@ from nltk.tokenize import word_tokenize
 #creating bag of words representation
 from sklearn.feature_extraction.text import CountVectorizer
 stop_words = set(stopwords.words('english')) 
-vectorizer = CountVectorizer(stop_words = stop_words)
+vectorizer = CountVectorizer()
 
 X_train_vectorized = vectorizer.fit_transform(X_train)
 X_test_vectorized=vectorizer.transform(X_test)
 
+#from imblearn.over_sampling import RandomOverSampler
+#ros = RandomOverSampler(random_state=42, sampling_strategy={'confirm':6000,'affirm':2000, 'reqalts':4000, 'ack':2000,'repeat':2000, 'negate':750,'reqmore':500, 'deny':2000, 'restart':2000})
+#X_train_vectorized, y_train = ros.fit_resample(X_train_vectorized, y_train)
+
 #classifier logistic regression
 
-def plot_confusion_matrix(cm,target_names,title='Confusion matrix', cmap=None,normalize=False):
+#taken from https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels
+def plot_confusion_matrix(cm,target_names,y_predicted,title='Confusion matrix', cmap=None,normalize=False):
     """
     given a sklearn confusion matrix (cm), make a nice plot
 
@@ -103,10 +108,10 @@ def plot_confusion_matrix(cm,target_names,title='Confusion matrix', cmap=None,no
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
 
-
+    eval_metrics=EvalMetrics(y_predicted)
     plt.tight_layout()
     plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}\n{}'.format(accuracy, misclass,eval_metrics))
     plt.show()
 
 #convert list of duplicates into dict with k,v where k=label and v=count
@@ -119,17 +124,15 @@ def Convert(lst):
 #plot confusion matrix and print label counts
 def make_confusion_matrix(y_predicted):
     dictionary=Convert(y_test)
-    print(dictionary)
     #create confusion matrix and plot
     from sklearn.metrics import confusion_matrix
     labels_no_duplicates= list(dict.fromkeys(labels))
     cm=confusion_matrix(y_test,y_predicted, labels_no_duplicates)
-    plot_confusion_matrix(cm,labels_no_duplicates)
+    plot_confusion_matrix(cm,labels_no_duplicates, y_predicted)
     
 #get evaluation metrics
 def EvalMetrics(y_predicted):
     
-    make_confusion_matrix(y_predicted)
     
     from sklearn.metrics import recall_score
     mi_recall=recall_score(y_test, y_predicted, average="micro")
@@ -142,8 +145,8 @@ def EvalMetrics(y_predicted):
     from sklearn.metrics import f1_score
     mi_f_score= f1_score(y_test, y_predicted, average='micro')
     ma_f_score= f1_score(y_test, y_predicted, average='macro')
-    print("micro recall, precision and f_score:" ,mi_recall, mi_precision, mi_f_score)
-    print("macro recall, precision and f_score:" ,ma_recall, ma_precision, ma_f_score)
+    return {'mi_recall':round(mi_recall,4), 'mi_precision':round(mi_precision,4), 'mi_f_score':round(mi_f_score,4),'ma_recall':round(ma_recall,4), 'ma_precision':round(ma_precision,4), 'ma_f_score':round(ma_f_score,4)}
+    
 
 def TrainLogisticRegression():
     from sklearn.linear_model import LogisticRegression
@@ -152,7 +155,8 @@ def TrainLogisticRegression():
     
     y_pred=clf.predict(X_test_vectorized)
     accuracy=clf.score(X_test_vectorized,y_test)
-    EvalMetrics(y_pred)
+    
+    make_confusion_matrix(y_pred)
     return clf
 
 def TrainNeuralNetwork():
@@ -165,9 +169,18 @@ def TrainNeuralNetwork():
     EvalMetrics(y_pred)
     return clf
 
+def TrainSVM():
+    from sklearn import svm
+    clf = svm.SVC(class_weight='balanced')
+    clf.fit(X_train_vectorized, y_train)
+    
+    y_pred=clf.predict(X_test_vectorized)
+    accuracy=clf.score(X_test_vectorized,y_test)
+    EvalMetrics(y_pred)
+    return clf
 #user input
 while True:
-    choice = input("enter 1 for logistic regression, 2 for NN classifier, stop to stop: ")
+    choice = input("enter 1 for logistic regression, 2 for NN classifier, 3 for SVM, stop to stop: ")
     if (choice == "1"):
         #classify user utterance
         lr_clf=TrainLogisticRegression()
@@ -181,6 +194,16 @@ while True:
                 break
     elif (choice=="2"):
         nn_clf=TrainNeuralNetwork()
+        while True:
+            var=input("enter utterance or enter stop to exit: ")
+            if (var != "stop"):
+                x=vectorizer.transform([var])
+                y=nn_clf.predict(x)
+                print("you have entered a "+y[0]+" utterance")
+            else:
+                break
+    elif (choice=="3"):
+        nn_clf=TrainSVM()
         while True:
             var=input("enter utterance or enter stop to exit: ")
             if (var != "stop"):
