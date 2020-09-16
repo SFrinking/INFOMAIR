7ef48ff4983d0145d921feb3bc9806fcf5c144d2
Luca Lin <agahamsc@live.com>
2020-09-16 14:05:07 +0200

    beautified code, added comments

--- baseline.py
+++ baseline.py
@@ -1,33 +1,58 @@
-##import the data
+def open_dataset(filename):
+    """
+    Parameters
+    ----------
+    filename : TYPE
+        DESCRIPTION.
 
-labels = list()
-utterances = list()
+    Returns
+    -------
+    utterances : list
+        X.
+    labels : list
+        y.
 
-with open("dialog_acts.dat", "r") as infile:
-  for line in infile:
-    label_utterance = line.lower().split(" ", 1)
-    labels.append(label_utterance[0])
-    utterances.append(label_utterance[1])
+    """
+    labels = list()
+    utterances = list()
+    with open(filename, "r") as infile:
+        for line in infile:
+            label_utterance = line.lower().split(" ", 1)
+            labels.append(label_utterance[0])
+            utterances.append(label_utterance[1])
+        return utterances,labels
+
+
+
+def split_dataset(X,y):
+    from sklearn.model_selection import train_test_split
+    return train_test_split(X,y, test_size=0.15)
     
-    
-#split into train and test
-import numpy as np
-from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(utterances, labels, test_size=0.15)
 
-#function to convert list to dictionary
-def Convert(lst): 
-    counts = {}
+
+def convert_to_dict_freq(lst): 
+    """
+
+    Parameters
+    ----------
+    lst : list
+        list of labels.
+
+    Returns
+    -------
+    counts_dictionary : dict
+        k,v where k=label and v=frequency.
+
+    """
+    counts_dictionary = {}
     for i in lst:
-      counts[i] = counts.get(i, 0) + 1
-    return counts
+      counts_dictionary[i] = counts_dictionary.get(i, 0) + 1
+    return counts_dictionary
 
-#count majority in training set
-dict=Convert(y_train)
-print(dict)
-#get highest count in dictionary
-def GetHighest(dictionary):
+
+#get most frequent label and its frequency
+def get_highest(dictionary):
     count=0
     k=""
     for key,value in dictionary.items():
@@ -36,103 +61,178 @@ def GetHighest(dictionary):
             k=key
     return k,count
 
-highest_label, highest_count=GetHighest(dict)
 
 
-#classify test set
-correct=0
-incorrect=0
+def baseline_one(y_test, highest_label):
+    """
+    given a list of labels, classify everything in y_test as that label
 
-for y in y_test:
-    if y==highest_label:
-        correct+=1
-    else:
-        incorrect+=1
-print("accuracy of baseline: " + str(correct/(len(y_test))))
-#get user input, loop
+    Parameters
+    ----------
+    y_test : list
+        
+    highest_label : str
+        highest label in training set.
+
+    Returns
+    -------
+    correct, incorrect: int
+        how many occurrances correctly and incorrectly classified
+
+    """
+    correct=0
+    incorrect=0
+    
+    for y in y_test:
+        if y==highest_label:
+            correct+=1
+        else:
+            incorrect+=1
+    return correct, incorrect
+
+
 
 import nltk
 nltk.download("punkt")
 from nltk.tokenize import word_tokenize
 
-keywords_m = {"request": ["where", "what", "whats", "type", "phone", "number",  #meaningful categories
-                        "address", "postcode", "post code"], 
-            "reqalts": ["how about", "what about", "anything else"],
-            #"inform": ["restaurant", "food"],
-            "confirm": ["is it", "does it", "do they", "is there", "is that"]
-             }
+def get_key_words():
+    keywords_m = {"request": ["where", "what", "whats", "type", "phone", "number",  #meaningful categories
+                            "address", "postcode", "post code"], 
+                "reqalts": ["how about", "what about", "anything else"],
+                #"inform": ["restaurant", "food"],
+                "confirm": ["is it", "does it", "do they", "is there", "is that"]
+                 }
+                    
+        
+    keywords_ts = {"repeat": ["repeat", "back", "again"],                   #turn-service categories
+                "ack": ["okay", "kay", "ok", "fine"],
+                "deny": ["wrong", "dont want", "not"],
+                "reqmore": ["more"],
+                "affirm": ["yes", "right", "correct", "yeah", "uh huh"],
+                "negate": ["no"]
+                  }
                 
-    
-keywords_ts = {"repeat": ["repeat", "back", "again"],                   #turn-service categories
-            "ack": ["okay", "kay", "ok", "fine"],
-            "deny": ["wrong", "dont want", "not"],
-            "reqmore": ["more"],
-            "affirm": ["yes", "right", "correct", "yeah", "uh huh"],
-            "negate": ["no"]
-              }
-            
-keywords_ds = {"hello": ["hi", "hello"],                                #dialogue-service categories
-            "goodbye": ["goodbye", "good bye", "bye", "stop"],
-            "thankyou": ["thank", "thanks"],
-            "restart": ["start over", "reset", ],
-            "null": ["cough", "unintelligible", "tv_noise", "noise", 
-                     "sil", "sigh", "um"]
-              }
+    keywords_ds = {"hello": ["hi", "hello"],                                #dialogue-service categories
+                "goodbye": ["goodbye", "good bye", "bye", "stop"],
+                "thankyou": ["thank", "thanks"],
+                "restart": ["start over", "reset", ],
+                "null": ["cough", "unintelligible", "tv_noise", "noise", 
+                         "sil", "sigh", "um"]
+                  }
+    return keywords_m,keywords_ts,keywords_ds 
+
+
 
 def keyword_rule(phrase):
-    res = "inform"
+    """
+    Given a phrase, predict the label based on key-word matching rules
+
+    Parameters
+    ----------
+    phrase : str
+        user input.
+
+    Returns
+    -------
+    y_pred : str
+        prediction of this classifier.
+
+    """
+    y_pred = "inform"
     
     for key_dict in [keywords_m, keywords_ts, keywords_ds]:
         for k,v in key_dict.items():
             for keyword in v:
                 if len(keyword.split(' ')) > 1:
                     if keyword in phrase:
-                        res = k
+                        y_pred = k
                         break
                 else:
                     if keyword in word_tokenize(phrase):
-                        res = k
+                        y_pred = k
                         break
-    return res
+    return y_pred
 
-#evaluate rule-based classification
 
-allcounter = 0
-truecounter = 0
 
-with open("dialog_acts.dat", "r") as infile:
+def baseline_two(X):
+    """
+    predict X based on key-word matching rules
 
-    for line in infile:
-        splitlst = line.lower().split(" ", 1)
-        res = keyword_rule(splitlst[1])
-        allcounter += 1
-        if res == splitlst[0]:
-            truecounter +=1
-            #outfile.write(splitlst[1] + ' ' + splitlst[0] + " TRUE\n" )
-        #else:
-        #    outfile.write(res + ' ' + splitlst[1] + splitlst[0] + " FALSE\n" )
+    Parameters
+    ----------
+    X : list
+        list of sentences to predict.
+
+    Returns
+    -------
+    correct, incorrect: int
+        how many occurrances correctly and incorrectly classified
+
+    """
+    correct = 0
+    incorrect=0
+    
+    for n in range(len(X)):
+        y_pred=keyword_rule(X[n])
+        if y_pred == y[n]:
+            correct+=1
+        else:
+            incorrect+=1
+    return correct,incorrect
+      
+
+def user_utterance(x):
+    """
+    
+
+    Parameters
+    ----------
+    x : string or function
+        if string, use it as y_pred. If function, call function on phrase.
+
+    Returns
+    -------
+    y_pred : string
+        prediction.
+
+    """
+    while True:
+        phrase=input("enter utterance or enter stop to exit:\n")
+        if (phrase != "stop"):
+            y_pred=""
+            if callable(x):
+                y_pred=x(phrase)
+            else:
+                y_pred= x
             
-acc = truecounter/allcounter
-print("accuracy keyword matching = {}".format(acc))
+            print("You have entered a {} utterance".format(y_pred))
+            return y_pred
+        else:
+            break
 
+
+X,y= open_dataset("dialog_acts.dat")
+X_train, X_test, y_train, y_test = split_dataset(X,y)
 while True:
-    choice = input("press 1 for baseline, 2 for keyword matching:\n")
+    choice = input("Enter 1 for baseline, 2 for keyword matching, 'stop' to exit:\n")
     if (choice == "1"):
-        #classify user utterance
-        while True:
-            var=input("enter utterance or enter stop to exit:\n")
-            if (var != "stop"):
-                print("you have entered a "+highest_label+" utterance")
-            else:
-                break
+        dict=convert_to_dict_freq(y_train)
+        highest_label, highest_count=get_highest(dict)
+        correct, incorrect= baseline_one(y_test, highest_label)
+        acc = correct/len(y_test)
+        misclassification= incorrect/len(y_test)
+        print("accuracy,misclassification of keyword matching = {},{}".format(acc,misclassification))
+        y_pred=user_utterance(highest_label)
     elif (choice=="2"):
-        while True:
-            phrase = input("enter utterance or enter stop to exit:\n")
-            if phrase == 'stop':
-                break
-            else:
-                res = keyword_rule(phrase)
-                print("You have entered a {} utterance".format(res))
+        keywords_m,keywords_ts,keywords_ds = get_key_words()  
+        correct,incorrect=baseline_two(X)
+        acc = correct/len(X)
+        misclassification= incorrect/len(X)
+        print("accuracy,misclassification of keyword matching = {},{}".format(acc,misclassification))
+        y_pred=user_utterance(keyword_rule)
+        
     elif( choice=="stop"):
         break
 
